a1420585182108d1925be9e8c330318ba379fe99
Sam Naish <sam.naish92@googlemail.com>
2019-10-06 19:57:26 +0100

    added meta data for seo

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,5 @@
 import App, { Container } from 'next/app';
+import Head from 'next/head';
 
 import Navigation from '../components/Navigation';
 import Footer from '../components/Footer';
@@ -22,6 +23,12 @@ export default class Application extends App {
         const { Component } = this.props;
         return (
             <Container>
+                 <Head>
+                    <title>Asmac &amp; Co (Surfacing) Ltd</title>
+                    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                    <meta name="description" content="Surfacing contractors with over 30 years of experience."/>
+                    <meta name="keywords" content="asmac, surfacing, macadam, ilford, driveways, roofing, contractors"/>
+                </Head>
                 <div className="app">
                     <Navigation />
                     <main className="app__content">
